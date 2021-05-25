a362a93e82101f44dca3b4fecf7dc21b3688c6f6
Andrew Heath <a.j.heath@student.utwente.nl>
2021-05-25 12:00:14 +0200

    Deleted SET cen files

--- plot_creation_scripts/SET_centrality_historgrams/SET_Lap_Histogram.py
+++ plot_creation_scripts/SET_centrality_historgrams/SET_Lap_Histogram.py
@@ -2,8 +2,21 @@ import csv
 import matplotlib.pyplot as plt
 import numpy as np
 
+import collections
 
-for i in range(0,10):
-    with open(i+'laplacian.csv', newline=',') as f:
-        reader = csv.reader(f)
-        data = list(reader)
+
+
+# frequency = collections.Counter(read_csv())
+# print(read_csv()[0])
+# # print(frequency)
+data0 = np.genfromtxt('/media/andrew/Storage/MOD-12-TCS/Research Project/CenBench/results/node_centrality_scores_set/0laplacian.csv',delimiter='')
+data99 = np.genfromtxt('/media/andrew/Storage/MOD-12-TCS/Research Project/CenBench/results/node_centrality_scores_set/99laplacian.csv',delimiter='')
+
+# plt.hist(data0, density=False, label='Epoch 0') 
+plt.hist(data99, density=False, label='Epoch 99')  # density=False would make counts
+plt.ylabel('Frequency')
+plt.xlabel('Laplacian Centrality');
+# plt.savefig("./plot_creation_scripts/plots/lap_epoch_0_vs)_99.png")
+# plt.legend(loc='upper right')
+
+plt.show()
